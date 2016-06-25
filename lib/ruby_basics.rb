2c1fe8c575c8102449485d96f6432ced0bde55d0
Satu Barnhill <satu.barnhill@gmail.com>
2016-06-25 18:58:06 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+ quotient = num1 / num2
+ quotient
 end
 
 def assign_variable(value)
-  
+  value
 end
 
-def argue
-  
+def argue(argument)
+ argument
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+  return_phrase = "Nice"
 end
 
 def last_evaluated_value
-
+  result = "newbie"
+  result = "intermediate"
+  result = "professional"
+  result = "expert"
 end
 
-def pizza_party
-
+def pizza_party(toppings = "cheese")
+  toppings
 end
